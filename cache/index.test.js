dc37b9059fe3f5888266cf641112d65275d42387
Abraham <cedenoabraham@gmail.com>
2022-11-04 15:34:57 -0300

    update

--- cache/index.test.js
+++ cache/index.test.js
@@ -1,20 +1,31 @@
 const Cache = require("./index");
 
 describe("cache class", () => {
+  const fn = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([1, 2, 3, 4, 5]);
+      }, 500);
+    });
+  };
+
   test("make sure that it is a singleton", async () => {
-    const fn = () => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve([1, 2, 3, 4, 5]);
-        }, 500);
-      });
-    };
     const options = {
       exp: 1 * 60000, // one minute
     };
-    const cache = new Cache();
+    const cache = new Cache(options);
     await cache.get("numbers", fn);
 
     expect(cache.get("numbers")).toEqual(new Cache().get("numbers"));
+    cache.mem.clear();
+  });
+
+  test("calls the cache storage the second time", async () => {
+    const cache = new Cache();
+    const fn = jest.fn(() => Promise.resolve({ data: {} }));
+    await cache.get("numbers", fn);
+    await cache.get("numbers", fn);
+    await cache.get("numbers", fn);
+    expect(fn).toHaveBeenCalledTimes(1);
   });
 });
